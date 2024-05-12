69b2b18ec09983ad236f2e52608b4c63c3bfd8e9
Loy Hui Chien <132521921+kardios@users.noreply.github.com>
2024-05-12 22:39:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,24 +15,20 @@ MISTRAL_API_KEY = os.environ["GROQ_API_KEY"]
 mistral = Groq(api_key=MISTRAL_API_KEY)
 
 st.set_page_config(page_title="TurboChapeau", page_icon=":sunglasses:",)
-st.write("**TurboChapeau** : pdf to para in a flash")
+st.subheader("**TurboChapeau** : pdf to para in a flash")
 
 original_prompt = "You will be summarizing a given text. Here is the text to summarize:\n\n<text>{{TEXT}}</text>\n\nPlease follow these steps to generate a high-quality summary:\n\n1. Read the text carefully and thoroughly to ensure you fully understand its content.\n\n2. Identify the main ideas and key details presented in the text. Consider the overall purpose and message the author is trying to convey.\n\n3. Generate a concise and coherent summary that captures the essence of the text. Focus on the most important information and avoid including unnecessary details or repetition.\n\n4. Present your summary in a clear and organized way, ensuring that it flows logically from one point to the next.\n\nPlease present your output as a single paragraph."
-system_prompt = st.text_area("Default Prompt:", original_prompt)
+system_prompt = st.text_area("", original_prompt)
          
-uploaded_file = st.file_uploader("Upload a PDF:", type = "pdf")
+uploaded_file = st.file_uploader("", type = "pdf")
 raw_text = ""
 if uploaded_file is not None:
-  try:
-    doc_reader = PdfReader(uploaded_file)
-    with st.spinner("Extracting from PDF document..."):
-      for i, page in enumerate(doc_reader.pages):
-        text = page.extract_text()
-        if text:
-          raw_text = raw_text + text + "\n"
-  except:
-    st.error(" Error occurred when loading document", icon="🚨")
-
+  doc_reader = PdfReader(uploaded_file)
+  for i, page in enumerate(doc_reader.pages):
+    text = page.extract_text()
+    if text:
+      raw_text = raw_text + text + "\n"
+  
   if raw_text != "":
     try:
       with st.spinner("Running AI Model..."):
@@ -49,8 +45,8 @@ if uploaded_file is not None:
         end = time.time()
 
         container = st.container(border=True)
-        container.write(output_text)
-        container.write("Time to generate: " + str(round(end-start,2)) + " seconds")
+        container.subheader(output_text)
+        container.subheader("Time to generate: " + str(round(end-start,2)) + " seconds")
         bot.send_message(chat_id=recipient_user_id, text="TurboChapeau" + "\n\n" + output_text)
         st.download_button(':floppy_disk:', output_text)
     except:
